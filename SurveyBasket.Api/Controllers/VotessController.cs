a773ae2372f82a43e18b91320382681612ff3970
Muhammad Abd-Elgawad <muhammad@gamil.com>
2025-11-22 11:31:30 +0200

    Add VotessController for handling vote-related endpoints
    Introduced a new `VotessController` class to manage vote-related
    API endpoints under the `/api/polls/{pollId}/votes` route.
    The controller enforces authorization, retrieves the user ID
    from claims, and interacts with `IQuestionService` to fetch
    available votes for a poll. Added appropriate HTTP responses
    for success, duplicate votes, and not found errors. Included
    dependency injection for `IQuestionService` and used
    `System.Security.Claims` for user identification.

---
+++ SurveyBasket.Api/Controllers/VotessController.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+
+namespace SurveyBasket.Api.Controllers
+{
+    [Route("api/polls/{pollId}/votes")]
+    [ApiController]
+    [Authorize]
+    public class VotessController(IQuestionService questionService) : ControllerBase
+    {
+        private readonly IQuestionService _questionService = questionService;
+
+        [HttpGet("")]
+        public async Task<IActionResult> Start([FromBody] int pollId,CancellationToken cancellationToken)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = await _questionService.GetAvailableAsync(pollId, userId!, cancellationToken);
+
+            if (result.IsSuccess)
+                return Ok(result.Value);
+
+            return result.Error.Equals(VoteErrors.DuplicatedVote)
+                ? result.ToProblem(StatusCodes.Status409Conflict)
+                : result.ToProblem(StatusCodes.Status404NotFound);
+
+        }
+    }
+}
